246504a120855e4efc7f6d45d1883239110f1993
unknown <panyarat.n@ku.th>
2025-11-09 15:58:08 +0700

    product

--- app/page.js
+++ app/page.js
@@ -1,17 +1,63 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import FormComponent from "@/components/FormComponent";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+const router = useRouter();
+const [products, setProducts] = useState([]);
+const [filteredProduct, setFilterProduct] = useState([]);
+
+  const getAllProduct = async () => {
+  try {
+    const response = await fetch("https://dummyjson.com/products");
+
+    if (!response.ok) throw new Error("Failed");
+
+    const allProduct = await response.json();
+    setProducts(allProduct.products);
+    setFilterProduct(allProduct.products);
+  } catch (error) {
+    setProducts([]);
+    setFilterProduct([]);
+  }
+};
+
+console.log('-products-', products);
+console.log('-filteredProduct-', filteredProduct);
+
+useEffect(() => {
+  getAllProduct();
+  }, []);
+
+  const handleSearch =(text) => {
+    const filterProduct = products.filter((p) => 
+      p.title.toLowerCase().includes(text.toLowerCase()));
+
+    setFilterProduct(filterProduct);
+  }
+
   return (
     <div>
       <Header />
-      <FormComponent />
+      <FormComponent onSearch={handleSearch} />
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 m-10">
-        {/* Code */}
+        {filteredProduct.map((item) => (
+          <div key={item.id}
+            className="flex flex-col items-center border border-gray-300 rounded-3xl hover:shadow-lg cursor-pointer"
+            onClick={() => router.push(`/products/${item.id}`)}
+          >
+            <img
+              alt={item.title}
+              src={item.thumbnail}
+            />
+          <p className="text-[16px] font-bold">{item.title}</p>
+          <p className="text-[16px]">{item.price}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
